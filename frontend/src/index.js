9444208c6f4ee8f18481d2a13871b1d24a894dc4
julieconnors <juliana.g.connors@gmail.com>
2021-02-15 21:22:33 -0500

    update fetch to use oo js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -38,61 +38,18 @@ document.addEventListener("DOMContentLoaded", () => {
 function fetchRiders(ridersEndpoint) {
     fetch(ridersEndpoint)
     .then(res => res.json())
-    .then(json => json.data.forEach(obj => {
-        createTableRow(obj)
-        
+    .then(json => json.data.forEach(rider => {
+        let newRider = new Rider(rider, rider.attributes)
+
+        newRider.createTableRow()
     }))
     .catch(error => console.log(error))
-}
-
-// function addRiderToTable(riderObj){
-    // create table row, append to ranking table
-    // create td for each and append to table row
-// }
-
-function createTableRow(riderObj){
-    const rankingTable = document.getElementById("ranking-table")
-    const row = document.createElement("tr")
-
-    let ranking = document.createElement("td")
-    ranking.innerHTML = 1
-
-    let name = document.createElement("td")
-    name.innerHTML = riderObj.attributes.name
-
-    let country = document.createElement("td")
-    country.innerHTML = riderObj.attributes.country.name
-    
-    // let flagImage = document.createElement("img")
-    // flagImage.src = riderObj.attributes.country.flag_image
-    // flag.appendChild(flagImage) 
-
-    let imageCell = document.createElement("td")
-    let image = document.createElement("img")
-    image.src = riderObj.attributes.image
-    imageCell.appendChild(image)
-
-    let points = document.createElement("td")
-    points.innerHTML = riderObj.attributes.points
-
-    // addRiderToTable
-    row.appendChild(ranking)
-    row.appendChild(name)
-    row.appendChild(image)
-    row.appendChild(country)
-    // row.appendChild(flagimage)
-    row.appendChild(points)
-
-    // add row to table
-    rankingTable.appendChild(row)
-    
 }
 
 function mountSubmitListener() {
     const form = document.querySelector(".rider-form")
     form.addEventListener("submit", (e) => createRiderFormHandler(e))
 } 
-
 
 function createRiderFormHandler(e) {
     e.preventDefault()
